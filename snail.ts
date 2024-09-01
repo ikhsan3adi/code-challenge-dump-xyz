36c0e92f98fabe935ee4797d8be48756bd90266a
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2024-09-01 09:02:39 +0000

    Create/Modify snail.ts

---
+++ snail.ts
@@ -0,0 +1,22 @@
+// 4 kyu
+// https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/javascript
+
+const snail = function (array: number[][]) {
+  let n = array.length
+  const output: number[] = []
+  for (let i = 0; i < array.length * array.length; i++) {
+
+  }
+  // array.map((row, i) => {
+  //   row.map((value, j) => {
+  //   })
+  // })
+  return output
+}
+
+console.log(snail([
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+])
+)
